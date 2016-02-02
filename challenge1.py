2acfb7eec6dc98fbba901255c2678851fd67190b
PrincessPwnz0rz <jessicaaimeecrosby@hotmail.co.uk>
2016-02-02 22:23:38 +0000

    Challenge 1 Python

---
+++ challenge1.py
@@ -0,0 +1,5 @@
+import binascii
+string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+hexstring = string.decode("hex")
+base64 = binascii.b2a_base64(hexstring)
+print(base64)
